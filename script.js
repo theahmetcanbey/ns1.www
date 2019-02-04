84333392e307c9d69563221586c7e5708220c7f0
Ahmet Can <theahmetcanbey@gmail.com>
2019-02-04 08:01:33 +0000

    update

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 direct = new Vue({
 	el: "#direct",
-	methods: {
-		selectUser: function(a){
-			this.selectedUser = a;
-		}
-	},
-	beforeMount: function(){
-		this.selectedUser = this.users[0];
-	},
 	data: {
 		"me": {
 			"id": "1",
@@ -30,64 +22,50 @@ direct = new Vue({
 				"preview": "son mesaj 2"
 			}
 		],
-		"chats": [
+		"allMessages": [
 			{
 				"from":"2",
 				"to":"1",
-				"time":"2019-01-01 20:30",
+				"time":"20:30",
 				"message":"selam"
 			},
 			{
 				"from":"1",
 				"to":"2",
-				"time":"2019-01-01 20:30",
+				"time":"20:30",
 				"message":"a.selam"
 			},
 			{
 				"from":"3",
 				"to":"1",
-				"time":"2019-01-01 20:30",
+				"time":"20:30",
 				"message":"selam üçten"
 			},
 			{
 				"from":"1",
 				"to":"3",
-				"time":"2019-01-01 20:30",
+				"time":"20:30",
 				"message":"a.selam birden üçe"
 			}
 		],
-		selectedUser: null
+		selectedUser: {
+			"id": "0",
+			"img": "0",
+			"name": "0",
+			"time": "0",
+			"preview": "0"
+		}
+	},
+	computed: {
+		chats: {
+			cache: false,
+			get: function() {
+				return _.filter(direct.allMessages, function(o){
+					return o.from == direct.selectedUser.id || o.to == direct.selectedUser.id;
+				})
+			}
+		}
 	}
 });
 
-document.querySelector('.chat[data-chat=user2]').classList.add('active-chat');
-document.querySelector('.user[data-chat=user2]').classList.add('active');
-
-var friends = {
-		list: document.querySelector('ul.people'),
-		all: document.querySelectorAll('.left .user'),
-		name: ''
-	},
-	chat = {
-		container: document.querySelector('.container .right'),
-		current: null,
-		user: null,
-		name: document.querySelector('.container .right .top .name')
-	};
-
-friends.all.forEach(function(f) {
-	f.addEventListener('mousedown', function() {
-		f.classList.contains('active') || setAciveChat(f);
-	});
-});
-
-function setAciveChat(f) {
-	friends.list.querySelector('.active').classList.remove('active');
-	f.classList.add('active');
-	chat.current = chat.container.querySelector('.active-chat');
-	chat.user = f.getAttribute('data-chat');
-	chat.current.classList.remove('active-chat');
-	chat.container.querySelector('[data-chat="' + chat.user + '"]').classList.add('active-chat');
-	friends.name = f.querySelector('.name').innerText;
-	chat.name.innerHTML = friends.name;
-}
+direct.selectedUser = direct.users[0];
