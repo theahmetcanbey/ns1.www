05d6b87e7145cf9c32922a2be8b41994e21437b0
Ahmet Can <theahmetcanbey@gmail.com>
2019-02-04 14:34:17 +0000

    update

---
+++ data.js
@@ -0,0 +1,117 @@
+_emptyUser = {
+	"id": "0",
+	"img": "0",
+	"name": "0",
+	"time": "0",
+	"preview": "0"
+};
+_me = {
+	"id": "1",
+	"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+	"name": "Ahmet Can",
+};
+_users = [
+	{
+		"id": "2",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "Ali Can",
+		"time": "22:30",
+		"preview": "son mesaj"
+	},
+	{
+		"id": "3",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 3",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	},
+	{
+		"id": "4",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 4",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	},
+	{
+		"id": "5",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 5",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	},
+	{
+		"id": "6",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 6",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	},
+	{
+		"id": "7",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 7",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	},
+	{
+		"id": "8",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 8",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	},
+	{
+		"id": "9",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 9",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	},
+	{
+		"id": "10",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 9",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	},
+	{
+		"id": "11",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 9",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	},
+	{
+		"id": "12",
+		"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
+		"name": "XXX 9",
+		"time": "22:30",
+		"preview": "son mesaj 2"
+	}
+];
+_allMessages = [
+	{
+		"from":"2",
+		"to":"1",
+		"time":"20:30",
+		"message":"selam"
+	},
+	{
+		"from":"1",
+		"to":"2",
+		"time":"20:30",
+		"message":"a.selam"
+	},
+	{
+		"from":"3",
+		"to":"1",
+		"time":"20:30",
+		"message":"selam üçten"
+	},
+	{
+		"from":"1",
+		"to":"3",
+		"time":"20:30",
+		"message":"a.selam birden üçe"
+	}
+];
--- script.js
+++ script.js
@@ -1,68 +1,32 @@
 direct = new Vue({
 	el: "#direct",
 	data: {
-		"me": {
-			"id": "1",
-			"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
-			"name": "Ahmet Can",
-		},
-		"users": [
-			{
-				"id": "2",
-				"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
-				"name": "Ali Can",
-				"time": "22:30",
-				"preview": "son mesaj"
-			},
-			{
-				"id": "3",
-				"img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg",
-				"name": "Mehmet",
-				"time": "22:30",
-				"preview": "son mesaj 2"
-			}
-		],
-		"allMessages": [
-			{
-				"from":"2",
-				"to":"1",
-				"time":"20:30",
-				"message":"selam"
-			},
-			{
-				"from":"1",
-				"to":"2",
-				"time":"20:30",
-				"message":"a.selam"
-			},
-			{
-				"from":"3",
-				"to":"1",
-				"time":"20:30",
-				"message":"selam üçten"
-			},
-			{
-				"from":"1",
-				"to":"3",
-				"time":"20:30",
-				"message":"a.selam birden üçe"
-			}
-		],
-		selectedUser: {
-			"id": "0",
-			"img": "0",
-			"name": "0",
-			"time": "0",
-			"preview": "0"
+		"me": _me,
+		"allMessages": _allMessages,
+		selectedUser: _emptyUser
+	},
+	methods: {
+		changeUser: function(user){
+			self = this;
+			self.selectedUser = _emptyUser;
+			setTimeout(function(){
+				self.selectedUser = user;
+			}, 1);
 		}
 	},
 	computed: {
+		users: {
+			cache: false,
+			get: function() {
+				return _users;
+			}
+		},
 		chats: {
 			cache: false,
 			get: function() {
 				return _.filter(direct.allMessages, function(o){
 					return o.from == direct.selectedUser.id || o.to == direct.selectedUser.id;
-				})
+				});
 			}
 		}
 	}
